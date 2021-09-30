267d5b4965eff661d02c10c92082120d17156c65
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2021-09-30 21:56:53 +0300

    configured allowed hosts

--- commerce/settings.py
+++ commerce/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '6ps8j!crjgrxt34cqbqn7x&b3y%(fn
 # DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "commerce-lisniak.herokuapp.com", 
+    '127.0.0.1'
+]
 
 
 # Application definition
