45bce6ce501a83a80b6a5be8923df3b1e7444915
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2021-10-04 23:45:17 +0300

    try to fix dev env

--- commerce/settings.py
+++ commerce/settings.py
@@ -151,12 +151,12 @@ DATABASES['default'].update(db_from_env)
 
 
 
-# if os.environ.get('STORAGE', '') == 'CLOUDINARY':
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME',''),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY',''),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET',''),
-}
+if os.environ.get('STORAGE', '') == 'CLOUDINARY':
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME',''),
+        'API_KEY': os.environ.get('CLOUDINARY_API_KEY',''),
+        'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET',''),
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
